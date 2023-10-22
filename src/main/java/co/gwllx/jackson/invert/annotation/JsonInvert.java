5f75b05cd561f594ba3392674a3dc494639fa5df
Gareth Williamson <gwllx.dev@gmail.com>
2023-10-22 22:17:18 +0200

    Remove unnecessary element type

--- src/main/java/co/gwllx/jackson/invert/annotation/JsonInvert.java
+++ src/main/java/co/gwllx/jackson/invert/annotation/JsonInvert.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.RECORD_COMPONENT})
+@Target({ElementType.FIELD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface JsonInvert {
 }
